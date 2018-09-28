2a2e1d9f43104be4b0d6f5d691b8fa38355808cc
m3elliot <m3elliot@local>
2018-09-28 20:07:21 +0000

    put jupyter on port 8888

--- v1.0.0/1_0_0.sh
+++ v1.0.0/1_0_0.sh
@@ -33,10 +33,10 @@ mkdir ~/.jupyter/custom && cd ~/.jupyter/custom && curl -O https://raw.githubuse
 #cd ~/.jupyter && openssl req -x509 -batch  -nodes -days 999 -newkey rsa:2048 -keyout mykey.key -out mycert.pem
 #echo "c.NotebookApp.certfile = u'/home/jovyan/.jupyter/mycert.pem'" >> ~/.jupyter/jupyter_notebook_config.py
 #echo "c.NotebookApp.keyfile =  u'/home/jovyan/.jupyter/mykey.key'"  >> ~/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.ip = '*'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.ip = '*'" > ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.extra_static_paths = ['/home/jovyan/.js_files']" >> ~/.jupyter/jupyter_notebook_config.py
+#echo "c.NotebookApp.port = 80" >> ~/.jupyter/jupyter_notebook_config.py  
 # *NOTE: Delete line below in later versions
-echo "c.NotebookApp.port = 80" >> ~/.jupyter/jupyter_notebook_config.py  
 echo "c.NotebookApp.token = ''" >> ~/.jupyter/jupyter_notebook_config.py  
 
 # Load in welcome_to_io
