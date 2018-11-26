65f24aa079a01dc979cb1393253f20fd658e9544
pupster90 <m3elliot@eng.ucsd.edu>
2018-11-26 19:10:39 +0000

    changed permission for run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # The command below creates a docker images from the io docker file
-docker build —no-cache -t gcr.io/pupster900/io:latest . 
+docker build --no-cache -t gcr.io/pupster900/io:latest . 
 
 # The next commented lines save the docker image
 # docker tag gcr.io/pupster900/io:latest gcr.io/pupster900/io:3.0
