47acc3ac6ffe45caf5d39ee65d1c57f3643ad4ea
pupster90 <m3elliot@eng.ucsd.edu>
2018-12-05 09:41:10 +0000

    added save.sh

---
+++ save.sh
@@ -0,0 +1,9 @@
+
+# These lines save the docker image to google registry
+docker tag gcr.io/pupster900/io:latest gcr.io/pupster900/io:3.0
+docker push gcr.io/pupster900/io:latest
+docker push gcr.io/pupster900/io:3.0
+
+
+
+
