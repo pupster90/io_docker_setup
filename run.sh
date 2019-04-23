0dfb0bcdb88609dbd30520ca5e90437bcb08b0a8
Matthew Elliott <m3elliot@eng.ucsd.edu>
2019-04-23 12:36:24 -0700

    added commented version of new run command

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 docker run -it -p 8883:80 --name io-new --user root gcr.io/pupster900/io:latest /bin/bash 
 #password "Owdy150!" 
+# docker run -it -p 8890:80 -p 8891:7681  --name io-test2 --user root gcr.io/pupster900/io:v3.0.6 /bin/bash
