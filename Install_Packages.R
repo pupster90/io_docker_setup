5386c8636958a623b50149cca237ec4195c42234
Matthew Elliott <m3elliot@eng.ucsd.edu>
2019-04-23 12:15:27 -0700

    Added file Install_Packages.R

---
+++ Install_Packages.R
@@ -0,0 +1,6 @@
+# This files installs all the R packages added to io
+install.packages("shinythemes", repos = "http://cran.us.r-project.org")
+install.packages("V8", repos = "http://cran.us.r-project.org")
+install.packages("shinyjs", repos = "http://cran.us.r-project.org")
+install.packages("plotly", repos = "http://cran.us.r-project.org")
+install.packages("BiocManager", repos = "http://cran.us.r-project.org")
