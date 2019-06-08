2732904ccf7635cea181e0c47c87680113123424
Matthew Elliott <m3elliot@eng.ucsd.edu>
2019-06-08 09:47:31 -0700

    added guest password and login.html config stuff

--- Set_Computer.sh
+++ Set_Computer.sh
@@ -10,10 +10,12 @@ sed -i "s/{VERSION_NAME}/$VERSION_NAME/g"  ~/io_docker_setup/custom.js
 ### Setup Jupyter config file
 echo "c.NotebookApp.ip = '0.0.0.0'" > ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.extra_static_paths = ['/home/jovyan/.js_files']" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.extra_template_paths = ['/home/jovyan/.html_files','...html/templates','...html']" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.port = 80" >> ~/.jupyter/jupyter_notebook_config.py  
 echo "c.NotebookApp.token = ''" >> ~/.jupyter/jupyter_notebook_config.py  
 echo "c.NotebookApp.allow_root = True" >> ~/.jupyter/jupyter_notebook_config.py  
 echo "c.NotebookApp.allow_origin = '*'" >> ~/.jupyter/jupyter_notebook_config.py  
+echo "c.NotebookApp.password = 'sha1:3efc472a30b9:6320e471692e9224957bd4ea358116df997980ed'" >> ~/.jupyter/jupyter_notebook_config.py 
 
 ### Generic Software Update
 apt update
@@ -99,6 +101,7 @@ pip install keras
 
 # Setup hidden files
 mkdir ~/.js_files && mv ~/io_docker_setup/cytoscape.min.js ~/.js_files
+mkdir ~/.html_files && mv ~/io_docker_setup/login.html ~/.html_files
 mkdir ~/.jupyter/custom && mv ~/io_docker_setup/custom.js ~/.jupyter/custom 
 
 # Setup Password
