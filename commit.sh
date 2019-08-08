0e10a53567d4fe8dd6182f70deeabf43cc6c5667
Matthew Elliott <m3elliot@eng.ucsd.edu>
2019-08-07 18:09:34 -0700

    added extra expose statement

--- commit.sh
+++ commit.sh
@@ -1,2 +1,2 @@
 # This file commits a docker container running io into a form for publishing
-docker commit --change='CMD ["tini", "-g", "jupyter", "notebook"]' -c "EXPOSE 80" io-new  gcr.io/pupster900/io:v3.0.3
+docker commit --change='CMD ["tini", "-g", "jupyter", "notebook"]' -c "EXPOSE 80" -c "EXPOSE 8888" io-test  pupster90/io:v3.0.7
